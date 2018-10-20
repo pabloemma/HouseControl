c6e6e3ff1e0e906c9bdc24b512137b1d02e66a4a
andi klein <pabloemma@gmail.com>
2018-10-19 18:48:32 -0600

    orginal commit

---
+++ HouseControl1/src/maincontrol.py
@@ -0,0 +1,58 @@
+'''
+Created on Oct 18, 2018
+
+@author: klein
+'''
+
+import wx
+import os
+
+
+class MainControl(wx.App):
+
+    def __init__ (self,redirect=False, filename = None):
+        """
+        Not much doing here
+        """
+    
+    # next statement intializes and is needed for wx to provide the OnInit
+        wx.App.__init__(self,redirect)
+
+    def OnInit(self): 
+         # where on the screen we have the frame
+        self.winsize = (800,600) #size of the window
+        self.CalculatePosition()
+        
+        self.frame = wx.Frame(parent=None,id=-1,title= "House control",size=self.winsize,pos = self.winpos)
+        print " id of frame",self.frame.GetId()
+
+        
+        
+ 
+        self.frame.Show()
+        # make this the topwindow
+        self.SetTopWindow(self.frame)
+        print "I am in OnInit"
+        # temporarya dialog
+        return True
+
+    def CalculatePosition(self):
+        """calculates the position of the window
+        Currently I want to have it in the lower left corner
+        """
+        # first we get the screen size
+        width, height = wx.GetDisplaySize()
+        
+        # since wx python uses y direction as measured from the top we need to calculate
+        # the positions such that the lower edge of the frame is still on the screen
+        ypos = height-self.winsize[1]
+        self.winpos =(50,ypos)
+        return
+        
+        
+
+
+if __name__ == '__main__':
+    MC= MainControl(redirect=False) # this redirects out put into a wx python window.
+    MC.MainLoop()
+    pass
