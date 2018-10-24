17dae400bc61f6d9c11d8cef38bbcfc9c9eb3510
andi klein <pabloemma@gmail.com>
2018-10-24 14:40:45 -0600

    changed refercne to tank files

--- HouseControl1/src/TankControl.py
+++ HouseControl1/src/TankControl.py
@@ -12,7 +12,7 @@ import socket, errno
 import subprocess
 
 import sys
-sys.path.insert(0, "/Users/klein/git/tank/tank/src")
+sys.path.insert(0, "~/git/tank/tank/src")
 import data_exchange_threaded as DE
 
 
